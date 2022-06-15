7cab5632c68b6e7dd7c12761febacc7e151bd2f3
Sanyuaung <sanyuaung.ygn.mm.com>
2022-06-15 15:19:00 +0630

    issuing upi settlement query change

--- app/Models/ifc99c.php
+++ app/Models/ifc99c.php
@@ -14,7 +14,7 @@ class ifc99c extends Model
     {
         DB::statement(DB::raw('set @row:=0'));
         $ifc99c = DB::select('select substring(Field1,1,3) as Txn_code,substring(Field1,4,4) as bitmap,substring(Field1,8,19) as Cardno,
-        format(concat(substring(Field1,27,10)," . ",substring(Field1,37,2)),2)as Txn_Amount,substring(Field1,39,3) as Curr,
+        format(concat(substring(Field1,27,10),".",substring(Field1,37,2)),2)as Txn_Amount,substring(Field1,39,3) as Curr,
         substring(Field1,42,10) as Txn_datetime, substring(Field1,52,6) as trace_no,substring(Field1,58,6) as Auth_ID_resp,
         substring(Field1,64,4) as Date_Auth,substring(Field1,68,12) as RRN,substring(Field1,80,11) as Acq_ID,
         substring(Field1,91,11) as Forward_ID,substring(Field1,102,4) as merchant_type,substring(Field1,106,8) as Card_acceptor_id,
@@ -24,14 +24,22 @@ class ifc99c extends Model
         substring(Field1,228,1) as ID_GSCS,substring(Field1,229,2) as Txn_initial_channel, substring(Field1,231,1) as Txn_features,
         substring(Field1,232,3) as Txn_scenario,substring(Field1,235,5) as Reserved, substring(Field1,240,30) as other_info, 
         substring(Field1,270,3) as POS_entry_mode,substring(Field1,273,1) as Floor_limit,substring(Field1,274,2) as Type_PaymentService,
-        format(concat(substring(Field1,276,10)," . ",substring(Field1,286,2)),2) as Settlement_Amt,
+        format(concat(substring(Field1,276,10),".",substring(Field1,286,2)),2) as Settlement_Amt,
         substring(Field1,288,3) as settlement_curr,substring(Field1,291,8) as settlement_convert_rate,
-        format(concat(substring(Field1,299,10)," . ",substring(Field1,309,2)),2) as Cardholder_billamt,
+        format(concat(substring(Field1,299,10),".",substring(Field1,309,2)),2) as Cardholder_billamt,
         substring(Field1,311,3) as Cardholder_bill_curr,substring(Field1,314,8) as Cardholder_billing_convert_rate,
         substring(Field1,322,12) as Net_Fee_Amt,substring(Field1,334,3) as IRF_Curr,
         substring(Field1,337,8) as Exrate_RF_bill_to_settlement_curr,substring(Field1,345,3) as Abbrev_Foreign_institute,
-        substring(Field1,348,1) as mainland_china_txn_ind,format(concat(substring(Field1,349,10)," . ",
-        substring(Field1,359,2)),2) as Txn_fee,substring(Field1,361,29) as QRC_voucher_no,
+        substring(Field1,348,1) as mainland_china_txn_ind,format(concat(substring(Field1,349,10),".",substring(Field1,359,2)),2) as Txn_fee,
+        substring(Field1,361,29) as QRC_voucher_no,substring(Field1,390,7) as Reserved1,substring(Field1,397,16) as Applied_cryptogram,
+        substring(Field1,413,3) as POS_entry_mode1,substring(Field1,416,3) as Application_PAN_seq_num,substring(Field1,419,1) as Terminal_entry_capability,
+        substring(Field1,420,1) as IC_card_condition_code,substring(Field1,421,6) as Terminal_capabilities,substring(Field1,427,10) as Terminal_verification_results,
+        substring(Field1,437,8) as Unpredictable_number,substring(Field1,445,8) as Serial_number_of_interface_device,substring(Field1,453,64) as Issuing_bank_application_data,
+        substring(Field1,517,4) as Application_transaction_counter,substring(Field1,521,4) as Application_alternation_characteristic,
+        substring(Field1,525,6) as Transaction_date,substring(Field1,531,3) as Country_code_of_the_terminal,substring(Field1,534,42) as Script_result_of_the_card_Issuer,
+        substring(Field1,576,2) as Authorization_response_code,substring(Field1,578,2) as Transaction_category,substring(Field1,580,12) as Authorized_amount,substring(Field1,592,3) as Currency_code_transaction,
+        substring(Field1,595,2) as Cipher_text_information_data,substring(Field1,597,12) as Other_amount,substring(Field1,609,6) as Authentication_method_and_result_of_the_cardholder,substring(Field1,615,2) as Terminal_category,
+        substring(Field1,617,32) as Dedicated_document_name,substring(Field1,649,4) as Application_version_number,substring(Field1,653,8) as Transaction_serial_counter,substring(Field1,661,30) as Reserved2,
         (@row:=@row + 1) AS NO,filename from ifc99cs
         order by substring(Field1,8,19)');
         return $ifc99c;
--- resources/views/NewSwitch/UPI/UPIupload.blade.php
+++ resources/views/NewSwitch/UPI/UPIupload.blade.php
@@ -12,6 +12,7 @@
             <thead>
                 <tr>
                     <th scope="col">NO</th>
+                    <th scope="col">Txn_code</th>
                     <th scope="col">bitmap</th>
                     <th scope="col">Cardno</th>
                     <th scope="col">Txn_Amount</th>
@@ -39,12 +40,58 @@
                     <th scope="col">Txn_scenario</th>
                     <th scope="col">Reserved</th>
                     <th scope="col">other_info</th>
+                    <th scope="col">POS_entry_mode</th>
+                    <th scope="col">Floor_limit</th>
+                    <th scope="col">Type_PaymentService</th>
+                    <th scope="col">Settlement_Amt</th>
+                    <th scope="col">settlement_curr</th>
+                    <th scope="col">settlement_convert_rate</th>
+                    <th scope="col">Cardholder_billamt</th>
+                    <th scope="col">Cardholder_bill_curr</th>
+                    <th scope="col">Cardholder_billing_convert_rate</th>
+                    <th scope="col">Net_Fee_Amt</th>
+                    <th scope="col">IRF_Curr</th>
+                    <th scope="col">Exrate_RF_bill_to_settlement_curr</th>
+                    <th scope="col">Abbrev_Foreign_institute</th>
+                    <th scope="col">mainland_china_txn_ind</th>
+                    <th scope="col">Txn_fee</th>
+                    <th scope="col">QRC_voucher_no</th>
+                    <th scope="col">Reserved1</th>
+                    <th scope="col">Applied_cryptogram</th>
+                    <th scope="col">POS_entry_mode1</th>
+                    <th scope="col">Application_PAN_seq_num</th>
+                    <th scope="col">Terminal_entry_capability</th>
+                    <th scope="col">IC_card_condition_code</th>
+                    <th scope="col">Terminal_capabilities</th>
+                    <th scope="col">Terminal_verification_results</th>
+                    <th scope="col">Unpredictable_number</th>
+                    <th scope="col">Serial_number_of_interface_device</th>
+                    <th scope="col">Issuing_bank_application_data</th>
+                    <th scope="col">Application_transaction_counter</th>
+                    <th scope="col">Application_alternation_characteristic</th>
+                    <th scope="col">Transaction_date</th>
+                    <th scope="col">Country_code_of_the_terminal</th>
+                    <th scope="col">Script_result_of_the_card_Issuer</th>
+                    <th scope="col">Authorization_response_code</th>
+                    <th scope="col">Transaction_category</th>
+                    <th scope="col">Authorized_amount</th>
+                    <th scope="col">Currency_code_transaction</th>
+                    <th scope="col">Cipher_text_information_data</th>
+                    <th scope="col">Other_amount</th>
+                    <th scope="col">Authentication_method_and_result_of_the_cardholder</th>
+                    <th scope="col">Terminal_category</th>
+                    <th scope="col">Dedicated_document_name</th>
+                    <th scope="col">Application_version_number</th>
+                    <th scope="col">Transaction_serial_counter</th>
+                    <th scope="col">Reserved2</th>
+
                 </tr>
             </thead>
             <tbody>
                 @foreach ($ifc99c as $ifc99c)
                     <tr>
                         <td>{{ $ifc99c->NO }}</td>
+                        <td>{{ $ifc99c->Txn_code }}</td>
                         <td>{{ $ifc99c->bitmap }}</td>
                         <td>{{ $ifc99c->Cardno }}</td>
                         <td>{{ $ifc99c->Txn_Amount }}</td>
@@ -72,6 +119,51 @@
                         <td>{{ $ifc99c->Txn_scenario }}</td>
                         <td>{{ $ifc99c->Reserved }}</td>
                         <td>{{ $ifc99c->other_info }}</td>
+                        <td>{{ $ifc99c->POS_entry_mode }}</td>
+                        <td>{{ $ifc99c->Floor_limit }}</td>
+                        <td>{{ $ifc99c->Type_PaymentService }}</td>
+                        <td>{{ $ifc99c->Settlement_Amt }}</td>
+                        <td>{{ $ifc99c->settlement_curr }}</td>
+                        <td>{{ $ifc99c->settlement_convert_rate }}</td>
+                        <td>{{ $ifc99c->Cardholder_billamt }}</td>
+                        <td>{{ $ifc99c->Cardholder_bill_curr }}</td>
+                        <td>{{ $ifc99c->Cardholder_billing_convert_rate }}</td>
+                        <td>{{ $ifc99c->Net_Fee_Amt }}</td>
+                        <td>{{ $ifc99c->IRF_Curr }}</td>
+                        <td>{{ $ifc99c->Exrate_RF_bill_to_settlement_curr }}</td>
+                        <td>{{ $ifc99c->Abbrev_Foreign_institute }}</td>
+                        <td>{{ $ifc99c->mainland_china_txn_ind }}</td>
+                        <td>{{ $ifc99c->Txn_fee }}</td>
+                        <td>{{ $ifc99c->QRC_voucher_no }}</td>
+                        <td>{{ $ifc99c->Reserved1 }}</td>
+                        <td>{{ $ifc99c->Applied_cryptogram }}</td>
+                        <td>{{ $ifc99c->POS_entry_mode1 }}</td>
+                        <td>{{ $ifc99c->Application_PAN_seq_num }}</td>
+                        <td>{{ $ifc99c->Terminal_entry_capability }}</td>
+                        <td>{{ $ifc99c->IC_card_condition_code }}</td>
+                        <td>{{ $ifc99c->Terminal_capabilities }}</td>
+                        <td>{{ $ifc99c->Terminal_verification_results }}</td>
+                        <td>{{ $ifc99c->Unpredictable_number }}</td>
+                        <td>{{ $ifc99c->Serial_number_of_interface_device }}</td>
+                        <td>{{ $ifc99c->Issuing_bank_application_data }}</td>
+                        <td>{{ $ifc99c->Application_transaction_counter }}</td>
+                        <td>{{ $ifc99c->Application_alternation_characteristic }}</td>
+                        <td>{{ $ifc99c->Transaction_date }}</td>
+                        <td>{{ $ifc99c->Country_code_of_the_terminal }}</td>
+                        <td>{{ $ifc99c->Script_result_of_the_card_Issuer }}</td>
+                        <td>{{ $ifc99c->Authorization_response_code }}</td>
+                        <td>{{ $ifc99c->Transaction_category }}</td>
+                        <td>{{ $ifc99c->Authorized_amount }}</td>
+                        <td>{{ $ifc99c->Currency_code_transaction }}</td>
+                        <td>{{ $ifc99c->Cipher_text_information_data }}</td>
+                        <td>{{ $ifc99c->Other_amount }}</td>
+                        <td>{{ $ifc99c->Authentication_method_and_result_of_the_cardholder }}</td>
+                        <td>{{ $ifc99c->Terminal_category }}</td>
+                        <td>{{ $ifc99c->Dedicated_document_name }}</td>
+                        <td>{{ $ifc99c->Application_version_number }}</td>
+                        <td>{{ $ifc99c->Transaction_serial_counter }}</td>
+                        <td>{{ $ifc99c->Reserved2 }}</td>
+
                     </tr>
                 @endforeach
             </tbody>
